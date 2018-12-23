68bf5a2cf1588b7be49c3c75519d673abdfa5734
jokerl <1456455229@qq.com>
2018-12-23 21:18:00 +0800

    test

---
+++ php/php7新特性/Test.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: JokerL
+ * Date: 2018/12/23
+ * Time: 14:02
+ */
+class Test
+{
+
+    CONST PHP7_ARRAY_CONST = [
+       'AA','BB','CC'
+    ];
+
+     public function go()
+     {
+         goto AA;
+
+         AA:
+
+     }
+
+     public function get($file):resource
+     {
+         $handle = fopen($file, 'r');
+         if ($handle !== false)
+         {
+             return $handle;
+         }
+     }
+
+}
---
+++ php/php7新特性/index.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: JokerL
+ * Date: 2018/12/23
+ * Time: 14:08
+ */
+
+require_once 'Test.php';
+
+ var_dump(Test::PHP7_ARRAY_CONST);
+
+ const A = [1];
+
+ define('B',[222]);
+
+ var_dump(A,B);
+
+ class B{
+     function __construct(float $a)
+     {
+     }
+ }
+
+
+$r = (new Test())->get(__DIR__.'/test');
+
+ var_dump($r);
